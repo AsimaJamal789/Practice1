f80b2da071a2ed084b583a52d86e01af9304be93
syeda areej <syedaareej789@gmail.com>
2021-03-14 20:27:03 +0500

    first commit

---
+++ Practice1.py
@@ -0,0 +1,12 @@
+"""for printing statment or string
+writing our first python program  (multi line comment)"""
+
+print("My name is Areej")
+
+#this comma will print a space(single line comment)
+print("I am student of CSE","My RollNo is 25")
+
+print("But I like Army", end=",") # for joining both the lines as by default print statment in next line will give you next line 
+print("i want to join")
+
+print("Hello\nHow are you") # escape sequence charater \n for new line ,\t for tab space 
